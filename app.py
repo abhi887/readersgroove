ab72521ab1a38fab2e89d96fc2c94b1cbb624e3d
abhi887 <vyasa887@gmail.com>
2019-04-26 19:27:22 +0530

    app updated

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ app.static_folder='static'
 Session(app)
 global usrnm
 
-if __name__=='__main__':
-    app.run()
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route("/")
 def index():
